91a89d03a0846618089da4335419a80dfaed1ff2
diepdinhminh <diepdinhminh.thp@gmail.com>
2018-11-08 18:22:53 +0700

    #2 - design template - fix conflicts

--- resources/views/guests/template/header.blade.php
+++ resources/views/guests/template/header.blade.php
@@ -1,6 +1,6 @@
 <head>
 
-    <link href="css/gstyle.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
     <meta name="keywords"
           content="Mobilestore iphone web template, Android web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design"/>
     <link href='//fonts.googleapis.com/css?family=Londrina+Solid|Coda+Caption:800|Open+Sans' rel='stylesheet'
